a59df105b543a555c88c207ec69de8578da0ac82
유재성 <149031461+Peter-Yu-0402@users.noreply.github.com>
2024-11-01 03:38:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,15 +1,19 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-
-        String[][] hashMap = {{"zero","0"},{"one","1"},{"two","2"},
-                {"three","3"},{"four","4"},{"five","5"},{"six","6"},
-                {"seven","7"},{"eight","8"},{"nine","9"}};
-
-        for (int i = 0; i < hashMap.length; i++) {
-            s = s.replace(hashMap[i][0],hashMap[i][1]);
+        // 0부터 9까지의 숫자와 그에 대응하는 영단어를 배열에 저장합니다.
+        String[] numWords = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        // 각 영단어를 숫자로 대체하기 위해 반복문을 돌립니다.
+        for (int i = 0; i < numWords.length; i++) {
+            // s가 현재 영단어(numWords[i])를 포함하고 있는 동안 계속 반복합니다.
+            if (s.contains(numWords[i])) {
+                // 해당 영단어를 해당 숫자로 대체합니다.
+                s = s.replace(numWords[i], String.valueOf(i));
+            }
         }
-
-        return answer = Integer.parseInt(s);
+        
+        // 모든 영단어가 숫자로 변환된 후, 문자열을 정수로 변환하여 반환합니다.
+        int answer = Integer.parseInt(s);
+        return answer;
     }
 }
