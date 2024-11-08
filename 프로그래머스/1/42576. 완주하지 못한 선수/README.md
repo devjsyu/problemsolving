2ff6710083703db797bcd8af51e633a21b65b426
유재성 <149031461+Peter-Yu-0402@users.noreply.github.com>
2024-11-08 10:45:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.88 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 94.8 MB, 시간: 86.94 ms
+메모리: 95.9 MB, 시간: 89.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 21일 21:44:58
+2024년 11월 08일 10:45:13
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,35 +1,27 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
-     public String solution(String[] participant, String[] completion) {
-        String answer = "";
+    public String solution(String[] participant, String[] completion) {
+        // 참가자의 이름과 그 수를 저장할 해시맵 생성
+        HashMap<String, Integer> participantMap = new HashMap<>();
 
-        LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
+        // 참가자 배열을 순회하여 각 참가자의 이름과 횟수를 맵에 저장
+        for (String p : participant) {
+            participantMap.put(p, participantMap.getOrDefault(p, 0) + 1);
+        }
 
-        for(int i = 0; i < participant.length; i++) {
-            if(map.containsKey(participant[i])){
-                map.put(participant[i], map.get(participant[i]) + 1);
-            } else {
-                map.put(participant[i], 1);
+        // 완주자 배열을 순회하며 완주한 참가자의 횟수를 감소
+        for (String c : completion) {
+            participantMap.put(c, participantMap.get(c) - 1);
+        }
+
+        // 참가자 해시맵에서 값이 0이 아닌 참가자를 찾아 반환
+        for (String key : participantMap.keySet()) {
+            if (participantMap.get(key) != 0) {
+                return key; // 완주하지 못한 참가자의 이름 반환
             }
         }
 
-        for(int i = 0; i < completion.length; i++) {
-            if(map.containsKey(completion[i])) {
-                if (map.get(completion[i]) == 1) {
-                    map.remove(completion[i]);
-                } else {
-                    map.replace(completion[i], map.get(completion[i]) - 1);
-                }
-            }
-        }
-
-        Set<String> keySet = map.keySet();
-
-        for (String key : keySet) {
-            answer =  key;
-        }
-
-        return answer;
+        return ""; // 이 부분은 논리적으로 실행되지 않지만 문법적 완전성을 위해 추가
     }
 }
