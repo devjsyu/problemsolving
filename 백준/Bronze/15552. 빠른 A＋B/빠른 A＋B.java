4c4c33699f0d45a1e6809af945b03e434fb0cdb0
유재성 <149031461+peter-jaesungyu@users.noreply.github.com>
2025-01-05 16:15:37 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1604 ms, Memory: 309364 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -2,36 +2,35 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Creating objects for InputStreamReader, OupputStreamWriter
-        InputStreamReader inputStreamReader = new InputStreamReader(System.in); // byte to string from console input
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out); // OutputStreamWriter OR just OutputStream 'cause we have PrintWriter?
-        PrintWriter printWriter = new PrintWriter(outputStreamWriter);
+        // try-with-resources for auto close
+        try (
+            // Creating an object for InputStreamReader, OupputStreamWriter
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in); // byte to string from console input
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out);
 
-        // Add BuffuredReader, BufferedWriter to speed up the job
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        BufferedWriter bufferedWriter = new BufferedWriter(printWriter);
+            // Add BuffuredReader, PrintWriter(BufferedWriter) to speed up the job
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            PrintWriter printWriter = new PrintWriter(outputStreamWriter)
+        ) {
+            // Taking integer input for loop size
+            int loopSize = Integer.parseInt(bufferedReader.readLine());
 
-        // Taking integer input for loop size
-        int loopSize = Integer.parseInt(bufferedReader.readLine());
+            // ForLoop
+            for (int i = 0; i < loopSize; i++) {
+                // Taking string input
+                String inputString = bufferedReader.readLine();
 
-        // ForLoop
-        for (int i = 0; i < loopSize; i++) {
-            // Taking string input
-            String inputString = bufferedReader.readLine();
+                // Split and sum
+                String[] splitString = inputString.split("\\s");
+                int parsedIntA = Integer.parseInt(splitString[0]);
+                int parsedIntB = Integer.parseInt(splitString[1]);
+                int output = parsedIntA + parsedIntB;
 
-            // Split and parse into int
-            int parsedIntA = Integer.parseInt(inputString.split("\\s")[0]);
-            int parsedIntB = Integer.parseInt(inputString.split("\\s")[1]);
-
-            // Sum and convert into String
-            String outputString = String.valueOf((parsedIntA + parsedIntB));
-
-            // Write outputString
-            printWriter.println(outputString);
+                // Write output
+                printWriter.println(output);
+            }
+            // Output resource flush
+            printWriter.flush();
         }
-        // Input resource flush and close
-        outputStreamWriter.flush(); outputStreamWriter.close(); bufferedReader.close();
-        // Output resource flush and close
-        printWriter.flush(); printWriter.close(); bufferedWriter.flush(); bufferedWriter.close();
     }
 }
