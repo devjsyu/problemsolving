517de52c5a1410cbb33b229f89ac2d15f9027acf
유재성 <149031461+peter-jaesungyu@users.noreply.github.com>
2025-01-06 12:02:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a Scanner object for input
-        Scanner scanner = new Scanner(System.in);
+        // try-with-resources for auto close
+        try (
+            // Create a BufferedReader class for faster input
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            // Assign variables
+            String string = br.readLine();
+            int index = Integer.parseInt(br.readLine()) - 1;
 
-        // Assign variables
-        String string = scanner.nextLine();
-        int index = scanner.nextInt() - 1;
-
-        // Prints charAt
-        System.out.println(string.charAt(index));
+            // Prints charAt
+            System.out.println(string.charAt(index));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
