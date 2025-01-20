1460ffac4db11e1d407b3944f9375e7251079fce
유재성 <149031461+peter-jaesungyu@users.noreply.github.com>
2025-01-20 18:16:07 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,26 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // Taking input
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        scanner.nextLine(); // reset new line
+    public static void main(String[] args) throws IOException {
+        // try-with resources for auto closing
+        try (
+                // Instantiate buffered IO class
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out)))
+        {
+            int size = Integer.parseInt(bufferedReader.readLine());
 
-        // StringBuilder to store results
-        StringBuilder stringBuilder = new StringBuilder();
+            // StringBuilder to store results
+            StringBuilder stringBuilder = new StringBuilder();
 
-        // for loop
-        for (int i = 0; i < size; i++) {
-            String s = scanner.nextLine();
-            for (int j = 2, n = s.length(); j < n; j++) {
-                String repeatedString = String.valueOf(s.charAt(j)).repeat(Character.getNumericValue(s.charAt(0)));
-                stringBuilder.append(repeatedString);
+            // for loop
+            for (int i = 0; i < size; i++) {
+                String s = bufferedReader.readLine();
+                for (int j = 2, n = s.length(); j < n; j++) {
+                    String repeatedString = String.valueOf(s.charAt(j)).repeat(Character.getNumericValue(s.charAt(0)));
+                    stringBuilder.append(repeatedString);
+                }
+                stringBuilder.append("\n");
             }
-            stringBuilder.append("\n");
-        }
 
-        // Printing the results all at once
-        System.out.println(stringBuilder);
+            // Printing the results all at once
+            printWriter.println(stringBuilder);
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
     }
 }
