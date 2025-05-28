fb04db402733e25e5ac973425615540ce29a493d
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2025-05-28 15:27:01 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 188 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
-// 재귀를 이용하여 하위 문제로 쪼개가며 해결하기
-// 최적부분구조이지만 중복부분문제가 아니기 때문에 효율적으로 Dynamic Programming, Memoization을 사용할 수 없다
+// 불필요한 반복 재귀 호출 대신 반복문 활용
+// Dynamic Programming (Bottom up Tabulation)
+// 반복문으로 작은 문제부터 계산하며 2차원 배열 미리 완성시키고 입력값에 따라 참조 후 정답 출력하기
 public class Main {
     public static void main(String[] args) {
+        int[][] arr = new int[15][15];
+
+        for (int i = 1; i <= 14; i++) {
+            arr[0][i] = i;
+            arr[i][1] = 1;
+        }
+
+        for (int i = 1; i <= 14; i++) {
+            for (int j = 2; j <= 14 ; j++) {
+                arr[i][j] = arr[i][j-1] + arr[i-1][j];
+            }
+        }
+
         Scanner sc = new Scanner(System.in);
         int total = sc.nextInt();
         for (int i = 0; i < total; i++) {
             int k = sc.nextInt();
             int n = sc.nextInt();
-            System.out.println(peopleCount(k, n));
+            System.out.println(arr[k][n]);
         }
-    }
-
-    private static int peopleCount(int k, int n) {
-        if (n == 1) return 1;
-        if (k == 1) return n * (n + 1) / 2;
-        return peopleCount(k, n - 1) + peopleCount(k - 1, n);
     }
 }
