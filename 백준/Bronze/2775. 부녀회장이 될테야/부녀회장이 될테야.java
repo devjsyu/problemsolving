89a7d33c67e525d9b275a94c10116f359f51f584
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2025-06-07 12:55:39 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 172 ms, Memory: 17504 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
 
-// 불필요한 반복 재귀 호출 대신 반복문 활용
-// Dynamic Programming (Bottom up Tabulation)
-// 반복문으로 작은 문제부터 계산하며 2차원 배열 미리 완성시키고 입력값에 따라 참조 후 정답 출력하기
+// Dynamic Programming
+// 부분구조 및 최적합 특징이 있기 때문에 동적계획법 Memoization을 이용할 수 있다.
+// Bottom up Tabulation Memoization
 public class Main {
     public static void main(String[] args) {
+        // 입력 받기
+        Scanner in = new Scanner(System.in);
+        int total = in.nextInt();
+
+        // (k, n)의 값을 2차원 배열에 미리 저장하기
+        // 2차원 배열 선언
         int[][] arr = new int[15][15];
 
-        for (int i = 1; i <= 14; i++) {
+        // 0층 (초기값)
+        for (int i = 0; i < 15; i++) {
             arr[0][i] = i;
-            arr[i][1] = 1;
         }
 
-        for (int i = 1; i <= 14; i++) {
-            for (int j = 2; j <= 14 ; j++) {
-                arr[i][j] = arr[i][j-1] + arr[i-1][j];
+        // 점화식
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15 ; j++) {
+                arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
             }
         }
 
-        Scanner sc = new Scanner(System.in);
-        int total = sc.nextInt();
+        // 차례대로 매개변수 입력 받아 해당 인덱스의 2차원 배열 값 출력하기
         for (int i = 0; i < total; i++) {
-            int k = sc.nextInt();
-            int n = sc.nextInt();
+            int k = in.nextInt();
+            int n = in.nextInt();
             System.out.println(arr[k][n]);
         }
     }
