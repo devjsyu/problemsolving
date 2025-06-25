f98e5735ed91b322267adb1ab335a0cfeb2bf27c
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2025-06-25 17:53:39 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 28212 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,46 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Optional;
+import java.io.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
             // 입력 받기
-            int n = Integer.parseInt(reader.readLine());
+            int n = Integer.parseInt(reader.readLine()); // 전체 개수
 
-            Deque<Integer> stack = new ArrayDeque<>(); // 스택
-            StringBuilder sb = new StringBuilder(); // 중간 연산과정 저장
-            int prev = 0; // 직전 숫자 저장
+            // 스택 객체 생성
+            Stack<Integer> stack = new Stack<>();
 
-            // n번 반복
+            // 정답 출력을 위한 StringBuilder 객체 생
+            StringBuilder sb = new StringBuilder();
+
+            // 수열에 대해 알고리즘 적용하기
+            int sequence = 0;
             for (int i = 0; i < n; i++) {
-                int curr = Integer.parseInt(reader.readLine()); // 현재 숫자
+                // 수 입력 받기
+                int num = Integer.parseInt(reader.readLine());
 
-                Optional<Integer> top = Optional.ofNullable(stack.peek()); // 스택 가장 위 원소
-                // 조기 종결 조건 검사
-                if (top.isPresent() && curr < prev && curr != top.get()) {
-                    System.out.println("NO");
-                    return;
-                }
-
-                // curr > prev인 경우
-                if (curr > prev) {
-                    for (int j = prev + 1; j <= curr; j++) {
-                        stack.push(j);
+                if (sequence < num) {
+                    for (int j = sequence; j < num; j++) {
+                        stack.push(++sequence);
                         sb.append('+').append('\n');
                     }
+                } else if (stack.peek() != num) {
+                    writer.write("NO");
+                    return; // 조기 반환
                 }
-                // curr > prev인 경우와 curr < prev인 경우 모두 포함
+                // 공통
                 stack.pop();
                 sb.append('-').append('\n');
-                prev = prev > curr ? prev : curr; // 다음 차례로 넘어가기 전, 현재 요소가 직전 요소보다 더 클 경우, 직전 요소 변수에 저장
             }
 
             // 정답 출력하기
-            System.out.println(sb);
+            writer.write(sb.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
