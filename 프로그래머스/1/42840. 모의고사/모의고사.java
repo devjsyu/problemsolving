0e818c1122676b215b1d3f447e8bd407b642c42e
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2025-06-26 10:32:32 +0900

    [level 1] Title: 모의고사, Time: 3.90 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,31 +1,54 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1,2,3,4,5}; // 5개씩 반복
-        int[] second = {2,1,2,3,2,4,2,5}; // 8개씩 반복
-        int[] third = {3,3,1,1,2,2,4,4,5,5}; // 10개씩 반복
-        int[] score = {0,0,0}; // 각 수포자들의 점수
-        
-        // 수포자들의 점수 계산
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == first[i%5]) score[0]++;
-            if(answers[i] == second[i%8]) score[1]++;
-            if(answers[i] == third[i%10]) score[2]++;
-        }
-        
-        // 최대 점수 구하기
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        
-        // 최대 점수를 가진 수포자 리스트 생성
-        List<Integer> answ = new ArrayList<Integer>();
-        for(int i=0; i<score.length; i++) if(max == score[i]) answ.add(i+1);
-        
-        int[] answer = new int[answ.size()];
-        for(int i=0; i<answ.size(); i++){
-            answer[i] = answ.get(i);
+        int[] student1 = {1, 2, 3, 4, 5};
+        int student1Cycle = student1.length;
+
+        int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int student2Cycle = student2.length;
+
+        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int student3Cycle = student3.length;
+
+        int[] answerMatchCount = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            // 수포자 1이 찍은 답안 매칭
+            int index = i % student1Cycle;
+            if (answers[i] == student1[index]) {
+                answerMatchCount[0]++;
+            }
+
+            // 수포자 2이 찍은 답안 매칭
+            index = i % student2Cycle;
+            if (answers[i] == student2[index]) {
+                answerMatchCount[1]++;
+            }
+
+            // 수포자 3이 찍은 답안 매칭
+            index = i % student3Cycle;
+            if (answers[i] == student3[index]) {
+                answerMatchCount[2]++;
+            }
         }
 
-        return answer;
+        // 정답 개수 최대값 찾기
+        int max = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answerMatchCount[i] > max) {
+                max = answerMatchCount[i];
+            }
+        }
+
+        // 최다 득점자를 오름차순으로 배열에 담아 반환하기
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (max == answerMatchCount[i]) {
+                list.add(i + 1);
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
